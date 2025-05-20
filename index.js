f2ebb84edcfc23ed79610302b59085a83484e224
ashutosh tiwari <ashutoshtiwarindia@gmail.com>
2025-05-20 11:58:21 +0000

    add client side message delivery

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ import { Server } from 'socket.io';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
-// open the database file
 const db = await open({
   filename: 'chat.db',
   driver: sqlite3.Database
 });
 
-// create our 'messages' table (you can ignore the 'client_offset' column for now)
 await db.exec(`
   CREATE TABLE IF NOT EXISTS messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,20 +31,26 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', async(socket) => {
-  socket.on('chat message', async (msg) => {
+  socket.on('chat message', async (msg, client_offset, callback) => {
     let result;
     try {
-      // store the message in the database
-      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, client_offset);
     } catch (e) {
-      // TODO handle the failure
+      if (e.errno === 19) {
+          if (typeof callback === 'function') {
+              callback();
+          }
+      } else {
+          console.error('Database error:', e);
+      }
       return;
     }
-    // include the offset with the message
     io.emit('chat message', msg, result.lastID);
+    if (typeof callback === 'function') {
+        callback();
+    }
   });
    if (!socket.recovered) {
-    // if the connection state recovery was not successful
     try {
       await db.each('SELECT id, content FROM messages WHERE id > ?',
         [socket.handshake.auth.serverOffset || 0],
@@ -55,7 +59,7 @@ io.on('connection', async(socket) => {
         }
       )
     } catch (e) {
-      // something went wrong
+      console.error(e)
     }
   }
 });
